310dab22c8df39871b3c1b62a46265721930202b
sunsik17 <17sunsik@gmail.com>
2025-10-28 13:46:11 +0900

    review: entity setter annotation

--- src/main/java/com/wjc/codetest/product/model/domain/Product.java
+++ src/main/java/com/wjc/codetest/product/model/domain/Product.java
@@ -7,6 +7,19 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+/* entity class에 setter를 사용하면 의도를 파악하기 어렵습니다.
+   필요할 때 사용하는 비즈니스 메소드를 만들어 사용하는 것이 좋은 방법 같습니다.
+ *
+ * public void setCategory(String category) {
+ *   this.category = category;
+ * } x
+ *
+ * public void modifyCategory(String category) {
+ *   this.category = category;
+ * } o
+ *
+ * 처럼 명확한 사용처를 알 수 있도록 하는 것이 좋을 것 같습니다.
+ */
 public class Product {
 
     @Id
