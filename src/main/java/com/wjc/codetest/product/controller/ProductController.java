c105d989b29cdeab0ed36923c59b7314dd551dd6
sunsik17 <17sunsik@gmail.com>
2025-10-29 20:38:06 +0900

    review: productController api endpoint review

--- src/main/java/com/wjc/codetest/product/controller/ProductController.java
+++ src/main/java/com/wjc/codetest/product/controller/ProductController.java
@@ -20,15 +20,16 @@ import java.util.List;
 - 원인 :
   - /get/product (동사 포함)
   - /create/product (동사 포함)
+  - /update/product (동사 포함)
+  - /delete/product (동사 포함)
   - /product/list (동사 미포함)
   - /product/category/list (동사 미포함)
 - 개선안(대안) :
 @RequestMapping("/products") 로 베이스 경로를 통일 하고
-@GetMapping("/{productId}")과 같이 http메소드로 행동을 표현하도록 url을 수정.
+@GetMapping("/{productId}")과 같이 동사 대신 http메소드로 행동을 표현하도록 url을 수정해야 합니다.
 
 - 선택 근거:
-RESTful 컨벤션 준수로 API 직관성이 좋아집니다.
-리소스 중심 URL을 설계 할 수 있습니다.
+RESTful api naming 컨벤션 준수로 API 직관성이 좋아집니다.
 HTTP 메서드로 액션을 구분할 수 있습니다.
  */
 @RequiredArgsConstructor
@@ -47,7 +48,7 @@ public class ProductController {
         return ResponseEntity.ok(product);
     }
 
-    @PostMapping(value = "/delete/product/{productId}")
+    @PostMapping(value = "/delete/product/{productId}") // 아래 updateProduct()와 같은 문제로 주석 하나로 대체합니다.
     public ResponseEntity<Boolean> deleteProduct(@PathVariable(name = "productId") Long productId){
         productService.deleteById(productId);
         return ResponseEntity.ok(true);
@@ -58,7 +59,16 @@ public class ProductController {
         Product product = productService.update(dto);
         return ResponseEntity.ok(product);
     }
-
+    /*
+    - 문제 : http 메소드를 잘못 사용하고 있습니다.
+    - 원인 : delete, update 작업에 PostMapping을 사용하고 있습니다. post 메소드는 비멱등 메소드로 새로운 리소스를 생성할 때 사용합니다.
+    - 개선안(대안) :
+    데이터를 삭제할 때는 @DeleteMapping,
+    수정이 필요할 때는 @PutMapping을 사용 하도록 합니다.
+    - 선택 근거 :
+    적절한 http 메소드를 사용해 api 의도가 명확해집니다.
+    또 멱등한 연산을 할 경우 put을 사용하는 것이 의미적으로 적절하다고 생각합니다.
+     */
     @PostMapping(value = "/product/list")
     public ResponseEntity<ProductListResponse> getProductListByCategory(@RequestBody GetProductListRequest dto){
         Page<Product> productList = productService.getListByCategory(dto);
