902e643e8c79297b0eaee5977a0594773defe64d
sunsik17 <17sunsik@gmail.com>
2025-11-02 22:53:36 +0900

    review: request dto review

--- src/main/java/com/wjc/codetest/product/model/request/CreateProductRequest.java
+++ src/main/java/com/wjc/codetest/product/model/request/CreateProductRequest.java
@@ -18,4 +18,12 @@ public class CreateProductRequest {
         this.name = name;
     }
 }
-
+/*
+- 문제 : 보안 취약점 문제
+- 원인 : 데이터 검증이 전혀 이루어 지지 않고 있습니다.
+- 개선안 :
+@NotNull, @Size 등 검증 규칙 추가
+- 선택근거 :
+null, 빈 값을 방어할 수 있습니다.
+과도한 길이의 요청이나 비즈니스 요구사항을 지킬 수 있습니다.
+ */
