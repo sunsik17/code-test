07f830a87d413a8baf68be30d1a85159bc30d08f
sunsik17 <17sunsik@gmail.com>
2025-11-02 22:58:03 +0900

    review: modify request dto review

--- src/main/java/com/wjc/codetest/product/model/request/CreateProductRequest.java
+++ src/main/java/com/wjc/codetest/product/model/request/CreateProductRequest.java
@@ -23,6 +23,8 @@ public class CreateProductRequest {
 - 원인 : 데이터 검증이 전혀 이루어 지지 않고 있습니다.
 - 개선안 :
 @NotNull, @Size 등 검증 규칙 추가
+이후
+Controller 레벨에서 @Valid 로 유효성검증
 - 선택근거 :
 null, 빈 값을 방어할 수 있습니다.
 과도한 길이의 요청이나 비즈니스 요구사항을 지킬 수 있습니다.
