2e909df2b7baff9862aa8992383c7880421f0e70
sunsik17 <17sunsik@gmail.com>
2025-11-02 22:47:24 +0900

    review: product repository review

--- src/main/java/com/wjc/codetest/product/repository/ProductRepository.java
+++ src/main/java/com/wjc/codetest/product/repository/ProductRepository.java
@@ -22,6 +22,20 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     - 선택 근거 :
     메서드명과 파라미터명이 일치하면 코드의 의도를 더 직관적으로 전달할 수 있습니다.
     다른 개발자가 봐도 'category를 기준으로 상품을 조회한다'는 것을 바로 이해할 수 있습니다.
+
+    - 문제 2: 성능 문제
+    - 원인 : Page객체를 반환하기 위해 hibernate가 count쿼리를 발생
+    - 개선안:
+    offset, limit을 이용한 페이지네이션 -> where, limit 이용한 페이지네이션으로 변경
+    - 선택 근거 :
+    Page객체를 반환하기 위해 hibernate가 조회 할때마다 count쿼리를 발생시켜 비효율적입니다.
+    category로 필터링한 모든 product를 조회하는 것이기 때문에, 서비스가 커지면 당연히 대용량데이터가 될 듯 합니다.
+    수 백만건의 데이터중 마지막 페이지를 조회하려면 모든 데이터를 스캔해야 하기 때문에 성능상 문제가 예상 됩니다.
+    트레이드 오프 :
+    cursor 기반            <->         offset 기반
+    ui 제공 불가                        ui 제공 가능
+    초기비용 발생                       구현 쉬움
+    요청마다 성능 무관                   뒤로갈수록 느려짐 (최적화 필요)
      */
 
     @Query("SELECT DISTINCT p.category FROM Product p")
